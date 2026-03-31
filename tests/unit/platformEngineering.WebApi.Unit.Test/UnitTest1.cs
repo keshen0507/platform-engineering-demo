bfef8d7e7ecb681caad8829d3fc2aa28aebd7f37
keshen.govender@gmail.com <keshen0507@gmail.com>
2026-03-31 19:27:57 +0100

    Add tests

--- tests/unit/platformEngineering.WebApi.Unit.Test/UnitTest1.cs
+++ tests/unit/platformEngineering.WebApi.Unit.Test/UnitTest1.cs
@@ -1,4 +1,6 @@
-﻿namespace platformEngineering.WebApi.Unit.Test;
+﻿using NSubstitute;
+
+namespace platformEngineering.WebApi.Unit.Test;
 
 public class Tests
 {
@@ -11,5 +13,29 @@ public class Tests
     public void Test1()
     {
         Assert.Pass();
+    }
+
+    [Test]
+    public void weatherEndpoint_WhenTempZero_ShouldReturnWeatherForecast32()
+    {
+        //Arrange
+        var temp = 0;
+        
+        //Act
+        var result = new WeatherForecast(DateOnly.FromDateTime(DateTime.Now), temp, "");
+
+        Assert.That(result.TemperatureF, Is.EqualTo(32));
+    }
+    
+    [Test]
+    public void weatherEndpoint_WhenTemp100_ShouldReturnWeatherForecast212()
+    {
+        //Arrange
+        var temp = 100;
+        
+        //Act
+        var result = new WeatherForecast(DateOnly.FromDateTime(DateTime.Now), temp, "");
+
+        Assert.That(result.TemperatureF, Is.EqualTo(211));
     }
 }
